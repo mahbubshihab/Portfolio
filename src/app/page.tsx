88b6b0ab3dc0aba8c1a59016e405f6d15f951f10
xlshihab <xlshihab9@gmail.com>
2026-07-12 08:03:00 +0600

    Remove custom animated pointer cursor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { AnimatedCursor } from "@/features/home/AnimatedCursor";
+
 import { ScrollProgressPath } from "@/features/home/ScrollProgressPath";
 import { HeroSection } from "@/features/home/HeroSection";
 import { TechMarquee } from "@/features/home/TechMarquee";
@@ -10,7 +10,7 @@ import { BottomCTA } from "@/features/home/BottomCTA";
 export default function Home() {
   return (
     <main className="min-h-screen bg-background overflow-hidden selection:bg-primary/30 relative">
-      <AnimatedCursor />
+
       <ScrollProgressPath />
       
       <HeroSection />
